1885645620096da777f7b66398f426bbea4ccfca
Tomohito Ozaki <ozaki@yuroyoro.com>
2015-08-06 16:09:45 +0900

    [WIP] Added "--init" option to generator

--- lib/generators/administa/administa_generator.rb
+++ lib/generators/administa/administa_generator.rb
@@ -1,22 +1,31 @@
 class AdministaGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
+  argument :name,  type: :string, required: false, default: ""
+  class_option :init,       type: "boolean", default: false
   class_option :namespace,  type: "string", default: "", aliases: '-n'
   class_option :base_class, type: "string", default: "", aliases: '-b'
 
-  def initialize_options
-    if options[:namespace].blank?
-      options[:namespace] = Administa.config.namespace.to_s
+  def initialize(args, *options)
+    super
+
+    if !self.options[:init] && self.name.blank?
+      raise ArgumentError, "No value provided for required arguments 'name'"
     end
-    if options[:base_class].blank?
-      options[:base_class] = Administa.config.base_controller.to_s
+  end
+
+  def initialize_options
+    @options = options.dup
+    if @options[:namespace].blank?
+      @options[:namespace] = Administa.config.namespace.to_s
+    end
+    if @options[:base_class].blank?
+      @options[:base_class] = Administa.config.base_controller.to_s
     end
   end
 
   # connfig/initializers/administa.rb
   def create_initializers
-    puts options
-
     return if File.exist? "config/initializers/administa.rb"
 
     # generate
@@ -46,16 +55,21 @@ class AdministaGenerator < Rails::Generators::NamedBase
   end
 
   def append_menu
+    return if @options[:init]
+
+
     # append menu Administa::FooController
     insert_into_file "config/initializers/administa.rb",
-                     "    menu #{options[:namespace].camelize}::#{class_name.pluralize}Controller\n",
+                     "    menu #{@options[:namespace].camelize}::#{class_name.pluralize}Controller\n",
                      after: "config.menu do\n"
 
   end
 
   # app/controllers/administa/foo_controller.rb
   def generate_controller
-    dest = File.join("app", "controllers", options[:namespace])
+    return if @options[:init]
+
+    dest = File.join("app", "controllers", @options[:namespace])
 
     # create dir
     empty_directory dest unless File.exist? dest
--- lib/generators/administa/templates/controller.rb
+++ lib/generators/administa/templates/controller.rb
@@ -1,4 +1,4 @@
-class <%= "#{options[:namespace].camelize}::#{class_name.pluralize}Controller" %> < <%= options[:base_class] %>
+class <%= "#{@options[:namespace].camelize}::#{class_name.pluralize}Controller" %> < <%= @options[:base_class] %>
   include Administa::Controller
 
   administa model: <%= class_name %>
