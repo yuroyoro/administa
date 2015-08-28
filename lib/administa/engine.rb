8feb9e2da2aba3dd3fcbb9d9dd0f98c0695819f5
Tomohito Ozaki <ozaki@yuroyoro.com>
2015-08-28 14:59:23 +0900

    [WIP] Preload config if Rails.env is production

--- lib/administa/engine.rb
+++ lib/administa/engine.rb
@@ -3,5 +3,11 @@ module Administa
   class Engine < ::Rails::Engine
 
     isolate_namespace Administa
+
+    config.after_initialize do
+      if Rails.env.production?
+        Administa.config.initialize!
+      end
+    end
   end
 end
